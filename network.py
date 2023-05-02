319f3f5299f6d37454ffe3ea15a7ab4365d9dfb8
Markus Muschol <markus.muschol@denkwerk.com>
2023-05-02 20:45:13 +0200

    Add training and backpropagation method

--- network.py
+++ network.py
@@ -23,7 +23,17 @@ class NeuralNetwork:
         hidden_inputs = numpy.dot(self.wih, inputs)
         hidden_outputs = self.activation_function(hidden_inputs)
 
+        final_inputs = numpy.dot(self.who,hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        # backpropagation
+        output_errors = targets - final_outputs
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        self.who += self.learning_rate * numpy.dot((output_errors * final_outputs * 1.0 - final_outputs), numpy.transpose(hidden_outputs))
+        self.wih += self.learning_rate * numpy.dot((hidden_errors * hidden_outputs * 1.0 - hidden_outputs), numpy.transpose(inputs))
         pass
+
 
     def query(self, input_list):
         inputs = numpy.array(input_list, ndmin=2).T
